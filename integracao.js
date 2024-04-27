3b4dbb8960d66565247e45f76bfafe130d9f8b6b
Kab321 <85372838+Kab321@users.noreply.github.com>
2024-04-27 12:52:41 -0300

    Add files via upload

---
+++ integracao.js
@@ -0,0 +1,13 @@
+
+   
+    document.getElementById("btn").addEventListener("click", function() {
+    
+        window.location.href = "https://api.whatsapp.com/send/?phone=5511976559077&text=Preciso+de+auxílio+jurídico+&type=phone_number&app_absent=0"; 
+    });
+    
+   
+    document.getElementById("btn2").addEventListener("click", function() {
+
+        window.location.href = "https://www.instagram.com/sm.advocaciacriminal/"; 
+    });
+
